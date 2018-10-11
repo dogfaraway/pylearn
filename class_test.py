a9e24597b456f4431c69ddc943117d2729f033ec
dogfaraway <dogfaraway@hotmail.com>
2018-10-11 23:41:30 +0800

    append local training

--- class_test.py
+++ class_test.py
@@ -1,13 +1,26 @@
 class Player():
-    def __init__(self, name, hp):
-        self.name = name
+    def __init__(self, name, hp, occu):
+        self.__name = name
         self.hp = hp
+        self.occu = occu
 
     def print_role(self):
-        print('%s: %s' % (self.name, self.hp))
+        print('%s: %s: %s' % (self.__name, self.hp, self.occu))
 
+    def updateName(self, newname):
+        self.name = newname
 
-user1 = Player('tom', 100)
-user2 = Player('jerry', 80)
+class Monster():
+    '定义怪物类'
+
+    pass
+
+user1 = Player('tom', 100, 'warr')
+user2 = Player('jerry', 80, 'master')
 user1.print_role()
 user2.print_role()
+
+user1.updateName('wilson')
+user1.print_role()
+
+
