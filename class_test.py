421730dbd66c1a011e660d5972f3b81391a88af8
dogfaraway <dogfaraway@hotmail.com>
2018-10-12 22:26:05 +0800

    append local training

--- class_test.py
+++ class_test.py
@@ -10,17 +10,52 @@ class Player():
     def updateName(self, newname):
         self.name = newname
 
+
 class Monster():
     '定义怪物类'
 
-    pass
+    def __init__(self, hp):
+        self.hp = hp
 
-user1 = Player('tom', 100, 'warr')
-user2 = Player('jerry', 80, 'master')
-user1.print_role()
-user2.print_role()
+    def run(self):
+        print('移动到某个位置')
 
-user1.updateName('wilson')
-user1.print_role()
+    def whoami(self):
+        print('我是怪物父类')
 
 
+class Animals(Monster):
+    '普通怪物'
+
+    def __init__(self, hp=10):
+        super().__init__(hp)
+        # self.hp = hp
+
+
+class Boss(Monster):
+    'boss类怪物'
+
+    def __init__(self, hp=1000):
+        super().__init__(hp)
+
+    def whoami(self):
+        print('我是怪物我怕谁')
+
+
+a1 = Monster(200)
+print(a1.hp)
+print(a1.run())
+a2 = Animals(1)
+print(a2.hp)
+print(a2.run())
+
+a3 = Boss(800)
+print(a3.whoami())
+
+# user1 = Player('tom', 100, 'warr')
+# user2 = Player('jerry', 80, 'master')
+# user1.print_role()
+# user2.print_role()
+#
+# user1.updateName('wilson')
+# user1.print_role()
