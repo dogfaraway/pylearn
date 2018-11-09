b8d3f4cc34553581e78161b8acbb5da85537a369
dogfaraway <dogfaraway@hotmail.com>
2018-11-09 23:20:22 +0800

    append local training

--- re_test.py
+++ re_test.py
@@ -2,3 +2,14 @@ import re
 
 p = re.compile('ca*t')
 print(p.match('caaaaat'))
+
+##
+import re
+
+p = re.compile(r'(\d+)-(\d+)-(\d+)')
+print(p.match('2018-05-10').group(2))
+print(p.match('2018-05-10').groups())
+
+year, month, date = p.match('2018-05-10').groups()
+
+print(p.search('aa2018-05-10a'))
